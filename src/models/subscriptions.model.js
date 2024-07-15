dc22fb3bab22b320da2fa845c5e25bc3c73345cb
BuddhadebKoner <jeetkoner36@gmail.com>
2024-07-15 22:19:34 +0530

    feat: Add subscriptions model

---
+++ src/models/subscriptions.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionsSchema = new mongoose.Schema(
+   {
+      subscriber: {
+         type: Schema.Types.ObjectId, // one who is subscribing
+         ref: "User",
+      },
+      chanal: {
+         type: Schema.Types.ObjectId, // one who is being subscribed
+         ref: "User",
+      },
+   },
+   { timestamps: true }
+);
+
+export const Subscriptons = mongoose.model("Subscriptons", subscriptionsSchema);
